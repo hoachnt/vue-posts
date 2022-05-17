325382720ea217521adf9267d8eb2fdf2a2c871a
Nguyen Hoach <hoachprogrammer@gmail.com>
2022-05-17 07:20:09 +0500

    Add src/components/UI/MySelect.vue

---
+++ src/components/UI/MySelect.vue
@@ -0,0 +1,27 @@
+<template>
+  <select v-model="modelValue" @change="changeOption">
+    <option disabled value="">Select</option>
+    <option v-for="option in options" :key="option.value" :value="option.value">{{ option.name }}</option>
+  </select>
+</template>
+<script>
+export default {
+  name: 'my-select',
+  props: {
+    modelValue: {
+      type: String,
+    },
+    options: {
+      type: Array,
+      default: () => [],
+    }
+  },
+  methods: {
+    changeOption(event) {
+      this.$emit('update:modelValue', event.target.value)
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
